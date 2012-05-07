d4d9b6ce0ebcbf9c400bd5510a5041879db7778c
Brian.Bonsai <brianbonsai777@gmail.com>
2012-05-07 20:12:40 +0900

    no message

---
+++ iOSAutomatism-NonARC/Classes/RSSChannelManager.h
@@ -0,0 +1,13 @@
+//
+//  RSSChannelManager.h
+//  iOSAutomatism-NonARC
+//
+//  Created by 徹公 別府 on 12/05/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSSChannelManager : NSObject
+
+@end
---
+++ iOSAutomatism-NonARC/Classes/RSSChannelManager.m
@@ -0,0 +1,13 @@
+//
+//  RSSChannelManager.m
+//  iOSAutomatism-NonARC
+//
+//  Created by 徹公 別府 on 12/05/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "RSSChannelManager.h"
+
+@implementation RSSChannelManager
+
+@end
